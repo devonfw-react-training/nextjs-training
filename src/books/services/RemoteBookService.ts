0e663a1328bf5f337e088425b8a71048c2c0a35a
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2021-09-24 17:52:31 +0200

    API standalone server

--- src/books/services/RemoteBookService.ts
+++ src/books/services/RemoteBookService.ts
@@ -1,23 +1,26 @@
 import { Book } from "../Book";
 import { BooksService } from "./BooksService";
 
+const API_BASE = "http://localhost:3001";
 export class RemoteBooksService implements BooksService {
   findOne(id: number): Promise<Book> {
     if (id || id === 0) {
-      return fetch(`/api/book/${id}`).then((response) => response.json());
+      return fetch(`${API_BASE}/api/book/${id}`).then((response) =>
+        response.json()
+      );
     } else {
       return Promise.reject(`book with id: ${id} not found`);
     }
   }
 
   findAll(): Promise<Book[]> {
-    return fetch("/api/books").then((response) => response.json());
+    return fetch(`${API_BASE}/api/books`).then((response) => response.json());
   }
 
   save(bookToSave: Book): Promise<Book> {
     const updateExisting = bookToSave.id || bookToSave.id;
 
-    return fetch("/api/book", {
+    return fetch(`${API_BASE}/api/book`, {
       method: updateExisting ? "POST" : "PUT",
       headers: {
         "Content-Type": "application/json",
